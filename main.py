ad799cf3eb1e5e7028c50720097d224c661d8c09
adityarana <aditya.rk.rana@gmail.com>
2018-02-05 16:48:55 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+flag = True
+while (flag):
+	x = input("Enter a first number")
+	y = input("Enter a second number")
+	op = input("Enter an operation")
+	if (op == '+'):
+		sum = int(x) + int(y)
+		print(sum)
